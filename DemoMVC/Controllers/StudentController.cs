8a06b833bc01cba7b7de47e275b152fb047e49e7
unknown <phanmyle22102003@gmail.com>
2024-08-25 22:45:14 +0700

    Create_NetCoreMVC_Project

---
+++ DemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers;
+public class StudentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
